3242b1df86cc34e2027845a5a76cd1d56713982a
Robert Bojor <robert.bojor@gmail.com>
2021-03-11 14:25:35 +0200

    Streamlined the code so the developer doesn't have to add a SceneType and then add the cases in two other places for that SceneType enumeration. The only rule is to name the enumeration just like the file name of the scene in Unity.

--- Scene/SceneType.cs
+++ Scene/SceneType.cs
@@ -3,6 +3,8 @@ namespace UnityCore {
 
     namespace Scene {
 
+        // Note: Name your SceneType enumerations just like the scene files in your Unity Scenes folder
+        // i.e. MainMenuScene, Level01Scene, etc
         public enum SceneType {
             None,
             Menu,
